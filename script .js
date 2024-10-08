89fd246d12c55be7ad6443e8fe41085ff76d320a
MarouaBenAli2024 <benalimaroua132@gmail.com>
2024-10-08 12:48:19 +0100

    first commit

---
+++ script .js
@@ -0,0 +1,21 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  let colorBox = document.getElementById("color-box");
+  let changeColorBtn = document.getElementById("change-color-btn");
+
+ 
+  function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+ 
+  changeColorBtn.addEventListener("click", function () {
+    let newColor = getRandomColor();
+    colorBox.style.backgroundColor = newColor;
+  });
+});
